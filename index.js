0ccdc4b327342a75fb0ca204611a919da03539e7
abhi17bgp <iabhishekbgp21@gmail.com>
2026-02-24 18:14:14 +0530

    index.js updated

--- index.js
+++ index.js
@@ -27,11 +27,7 @@ async function initBrowser() {
   if (!browser) {
     browser = await puppeteer.launch({
       headless: "new",
-      userDataDir: "./automation-profile",
-      args: [
-        "--start-maximized",
-        "--disable-blink-features=AutomationControlled",
-      ],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     console.log("Browser initialized");
@@ -432,6 +428,8 @@ app.post("/agent", async (req, res) => {
    START SERVER
 =========================== */
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
